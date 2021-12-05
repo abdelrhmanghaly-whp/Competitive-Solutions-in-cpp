3b6ac312239cc38882c04ee2319f4c3e1a230f42
Ghaly <84441904+abdelrhmanghaly-whp@users.noreply.github.com>
2021-12-05 17:01:59 +0200

    Add files via upload

---
+++ CodeForces Problem S (unfixed).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // Problem S
+	int x, y, size, max;
+	// print all the odd numbers between x and y without printing x-y
+	// line
+	cout << "Enter your numbers:" << endl;
+	cin >> x;
+	cin >> y;
+	if (x >= 0, y >= 0);
+	{
+		if (x > max, x > max);
+		{
+			cout << "the numbers are:" << endl;
+
+
+			cout << " Numbers between the x and y execluding them are:" << endl;
+			cin >> size;
+			cout << " the odd numbers are between x - y " << endl;
+
+
+		}
+	}
+
+}
