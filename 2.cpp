7a4939a75f53b8cb2177e9ac530bf4371c4d2caa
Abdelrhman Ghaly <84441904+abdelrhmanghaly-whp@users.noreply.github.com>
2023-01-09 23:16:31 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+ #include iostream
+ using namespace std;
+        int main()
+       {
+        char M[100];
+        cinM;
+        double a,b,c;
+        cinab;
+        c=a+(b15)100;
+        coutfixed;
+        coutsetprecision(2)TOTAL = R$ cendl;
+        return 0;
+    }  
